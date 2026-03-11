4fcdf611edeb9454596b269de7a8bbeb7e317143
Kartikeya Pusapati <swagboss600@gmail.com>
2026-03-11 21:57:44 +0530

    [Kartikeya]:UC3: Insert contacts

---
+++ sql/uc3_insert_contacts.sql
@@ -0,0 +1,6 @@
+INSERT INTO AddressBook
+(firstName,lastName,address,city,state,zip,phoneNumber,email)
+VALUES
+('Kartikeya','Pusapati','Guduvancheri','Chennai','TN','603202','9876543210','kartik@email.com'),
+('Rahul','Sharma','Indira Nagar','Bangalore','KA','560038','9123456789','rahul@email.com'),
+('Anita','Reddy','Banjara Hills','Hyderabad','TS','500034','9988776655','anita@email.com');
